b3b0bea4acaca6f9cb9c69872bfd55ca49437d41
dingdangmao123 <3228936343@qq.com>
2017-03-23 10:01:03 +0800

    version 3

--- RedisTools/cacheRedis.java
+++ RedisTools/cacheRedis.java
@@ -1,53 +1,53 @@
-package RedisTools;
+	package RedisTools;
 
-import java.net.*;
-import java.nio.channels.SocketChannel;  
-import java.util.concurrent.*;
+	import java.net.*;
+	import java.nio.channels.SocketChannel;  
+	import java.util.concurrent.*;
 
-public class cacheRedis{
+	public class cacheRedis{
 
 
-public static LinkedBlockingQueue<Redis> cache;
+		public static LinkedBlockingQueue<Redis> cache;
 
-static{
-	cache=new LinkedBlockingQueue<Redis>();
-}
+		static{
+			cache=new LinkedBlockingQueue<Redis>();
+		}
 
-public static Redis get(){
+		public static Redis get(){
 
-		
-		if(cache.size()>0){
+			
+			if(cache.size()>0){
 
 				try{
 
-		  					return cache.take();
+					return cache.take();
 
-		  	}catch(Exception e){
+				}catch(Exception e){
 
-								RedisLog.log(null,"cache get exception");
-								System.out.println("cache get exception");
+					RedisLog.log(null,"cache get exception");
+					System.out.println("cache get exception");
 
-		  	}
+				}
+
+			}
+			return new Redis();
 
 		}
-		return new Redis();
+		public static void set(Redis msg){
 
-}
-public static void set(Redis msg){
+			if(cache.size()<100){
+				msg.cli=null;
+				msg.cmd=null;
+				try{
+					cache.put(msg);
+					
 
-		if(cache.size()<100){
-					msg.cli=null;
-					msg.cmd=null;
-					try{
-								cache.put(msg);
-		
+				}catch(Exception e){
 
-					}catch(Exception e){
+					RedisLog.log(msg.cli,"cache exception");
+					System.out.println("cache exception");
 
-								RedisLog.log(msg.cli,"cache exception");
-								System.out.println("cache exception");
-
-					}
+				}
 			}
+		}
 	}
-}
